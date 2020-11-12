99286152a5f9ecbeaea847e24f627d9bb494156b
JoshHambright <josh.hambright@gmail.com>
2020-11-12 18:30:08 -0500

    Post Controller

---
+++ SleepyBook.Services/PostService.cs
@@ -0,0 +1,29 @@
+﻿using SleepyBook.Data;
+using SleepyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyBook.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity = new Post()
+            {
+                Author = _userId;
+                Title = 
+            }
+        }
+    }
+}
